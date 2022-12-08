60fcfaf9b340e8b3adc578170427ae3c5d67c0ce
AleixMJ <aleixtechIT@gmail.com>
2022-12-08 13:29:45 +0000

    added error page and entry

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # wiki
 Harvard webcs50 - Project 1
+
+Specification:
+https://cs50.harvard.edu/web/2020/projects/1/wiki/
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,8 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    if util.get_entry(entry) == None:
+        return render(request, "encyclopedia/error.html")
+    else:
+        return render(request, "encyclopedia/entry.html")
